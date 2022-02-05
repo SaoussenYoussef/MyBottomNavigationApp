e18c17c1ffe27b949c7ec1776698b94921e62352
SaoussenYoussef <saoussen.hasni@gmail.com>
2022-02-05 17:28:16 +0100

    myFirstCommit

---
+++ app/src/main/java/fr/numerized/mybottomnavigationapp/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package fr.numerized.mybottomnavigationapp.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Eliott and Valentin");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
